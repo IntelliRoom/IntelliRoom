e5e39d9aa9385a5512803b99f726004fc5ec8d61
IntelliRoom <diazjenny105@gmail.com>
2025-12-07 23:45:40 -0800

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAv_ZS8PLXoTcDIXkE-deVKgTPgIUchKJc",
+    authDomain: "intelliroom-50474.firebaseapp.com",
+    databaseURL: "https://intelliroom-50474-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "intelliroom-50474",
+    storageBucket: "intelliroom-50474.appspot.com"
+};
