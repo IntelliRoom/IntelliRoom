2d3361246f5d3ad267e79f51d5c4d49f4b651d94
IntelliRoom <diazjenny105@gmail.com>
2025-12-08 03:27:58 -0800

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAv_ZS8PLXoTcDIXkE-deVKgTPgIUchKJc",
-    authDomain: "intelliroom-50474.firebaseapp.com",
-    databaseURL: "https://intelliroom-50474-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "intelliroom-50474",
-    storageBucket: "intelliroom-50474.appspot.com"
+  apiKey: "AIzaSyAv_ZS8PLXoTcDIXkE-deVKgTPgIUchKJc",
+  authDomain: "intelliroom-50474.firebaseapp.com",
+  databaseURL: "https://intelliroom-50474-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "intelliroom-50474",
+  storageBucket: "intelliroom-50474.firebasestorage.app",
+  messagingSenderId: "391776071622",
+  appId: "1:391776071622:web:e215eb5a58179881654cc7"
 };
 
