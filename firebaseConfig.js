8c7dfe7391b3c6f05657ed5e97dcc31d4110094b
IntelliRoom <diazjenny105@gmail.com>
2025-12-13 22:12:24 -0800

    Update firebaseConfig structure and properties
    Removed databaseURL and added measurementId to firebaseConfig.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAv_ZS8PLXoTcDIXkE-deVKgTPgIUchKJc",
   authDomain: "intelliroom-50474.firebaseapp.com",
-  databaseURL: "https://intelliroom-50474-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "intelliroom-50474",
   storageBucket: "intelliroom-50474.firebasestorage.app",
   messagingSenderId: "391776071622",
-  appId: "1:391776071622:web:e215eb5a58179881654cc7"
+  appId: "1:391776071622:web:e215eb5a58179881654cc7",
+  measurementId: "G-WY77EP7TVZ"
 };
